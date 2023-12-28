1cd2fa9dcf56114ec68adf25fb027ab62a334a17
Max Eakkabin Jaikeawma <eakkabin_it@outlook.co.th>
2023-12-28 15:19:36 +0700

    update file index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-import Flexbox from './Flexbox';
-import { name as appName } from './app.json';
+import { AppRegistry } from 'react-native'
+import Flexbox from './Flexbox'
+import { name as appName } from './app.json'
 
-AppRegistry.registerComponent(appName, () => Flexbox);
+AppRegistry.registerComponent(appName, () => Flexbox)
